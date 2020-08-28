6d9e9342cd8af9e9f4892ffd5918f92a1980f4ff
Jonh Edson <kylix@hotmail.com>
2020-08-28 17:51:28 -0300

    CRUD completo na memoria

--- DemoGitHub/Controllers/AlunosController.cs
+++ DemoGitHub/Controllers/AlunosController.cs
@@ -12,28 +12,8 @@ namespace DemoGitHub.Controllers
 {
     public class AlunosController : Controller
     {
-        private readonly AlunoRepository alunoRepository;
-        #region 0 - Criando um objeto e enviando para a View()
-        //public IActionResult Index()
-        //{
-        //    Aluno aluno = new Aluno()
-        //    {
-        //        AlunoId = 1,
-        //        Nome = "Infnet",
-        //        Campus = "Sao Jose",
-        //        Curso = "EDC",
-        //        Sexo = "Masculino"
-        //    };
-        //    return View(aluno);
-        //}
-        #endregion
 
-        #region 1 - Criar, Editar, Deletar e Buscar o Aluno na Lista - Códigos não são pertecentes ao Controller
-
-
-        #endregion
-
-        #region 2 - Ações do Controlador
+        #region Ações do Controlador
 
         #region Method Index -> GetAllAlunos - OK
         public ActionResult Index()
@@ -42,6 +22,7 @@ namespace DemoGitHub.Controllers
             //var alunos = new List<AlunoViewModel>();
 
             IEnumerable<Aluno> alunos = AlunoRepository.GetalunoList();
+
             IEnumerable<AlunoViewModel> alunosviewmodel = alunos.Select(x => new AlunoViewModel
             {
                 AlunoId = x.AlunoId,
@@ -52,89 +33,128 @@ namespace DemoGitHub.Controllers
             });
             return View(alunosviewmodel);
         }
-
         #endregion
 
         #region Methods Create
-        //public ActionResult Create()
-        //{
-        //    return View();
-        //}
+        public ActionResult Create()
+        {
+            return View();
+        }
 
-        //[HttpPost]
-        //public ActionResult Create(AlunoViewModel aluno) // Recolher dados do formulario
-        //{
-            
-        //    AlunoRepository.Create(aluno);
+        [HttpPost]
+        public ActionResult Create(Aluno aluno) // Recolher dados do formulario
+        {
 
-        //    return RedirectToAction("Index");
-        //}
+            AlunoRepository.Create(aluno);
+
+            return RedirectToAction("Index");
+        }
         #endregion
 
-        //#region Methods Edit
-        //public ActionResult Edit(int id)
-        //{
-        //    return View(BuscarId(id));
-        //}
-        //[HttpPost]
-        //public ActionResult Edit(int id, IFormCollection collection)
-        //{
-        //    try
-        //    {
-        //        Aluno aluno = new Aluno();
-        //        aluno.Nome = collection["Nome"];
-        //        aluno.Campus = collection["Campus"];
-        //        aluno.Curso = collection["Curso"];
-        //        aluno.Sexo = collection["Sexo"];
-        //        // aluno.Idade = Convert.ToInt32(collection["Idade"]);
+        #region Methods Edit
+        public ActionResult Edit(int id)
+        {
+            Aluno aluno = AlunoRepository.BuscarId(id);
+            AlunoViewModel alunoviewmodel = new AlunoViewModel
+            {
+                Nome = aluno.Nome,
+                Campus = aluno.Campus,
+                Curso = aluno.Curso,
+                Sexo = aluno.Sexo
 
-        //        EditAluno(id, aluno);
-        //        return RedirectToAction(nameof(Index));
+            };
+            return View(alunoviewmodel);
+        }
 
-        //    }
-        //    catch
-        //    {
-        //        return View();
-        //    }
-        //}
-        //#endregion
 
-        //#region Methods Delete
-        //public ActionResult Delete(int id)
-        //{
 
-        //    return View(BuscarId(id));
-        //}
+        [HttpPost]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                Aluno aluno = new Aluno();
+                aluno.Nome = collection["Nome"];
+                aluno.Campus = collection["Campus"];
+                aluno.Curso = collection["Curso"];
+                aluno.Sexo = collection["Sexo"];
+                
 
-        //[HttpPost]
-        //public ActionResult Delete(int id, IFormCollection collection)
-        //{
-        //    try
-        //    {
-        //        DeleteAluno(id);
+                AlunoRepository.EditAluno(id, aluno);
+                return RedirectToAction(nameof(Index));
 
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    catch
-        //    {
-        //        return View();
-        //    }
-        //}
-        //#endregion
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        #endregion
 
-        //#region Method Details
-        //public ActionResult Details(int id)
-        //{
-        //    return View(BuscarId(id));
-        //}
-        //#endregion
+        #region Methods Delete
+        public ActionResult Delete(int id)
+        {
+            Aluno aluno = AlunoRepository.BuscarId(id);
+            AlunoViewModel alunoviewmodel = new AlunoViewModel
+            {
+                AlunoId = aluno.AlunoId,
+                Nome = aluno.Nome,
+                Campus = aluno.Campus,
+                Curso = aluno.Curso,
+                Sexo = aluno.Sexo
 
-        //#region Methods Buscar
-        //public ActionResult Buscar()
-        //{
-        //    string pesquisa = "";
-        //    return View(BuscarAluno(pesquisa));
-        //}
+            };
+            return View(alunoviewmodel);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                AlunoRepository.DeleteAluno(id);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        #endregion
+
+        #region Method Details
+        public ActionResult Details(int id)
+        {
+            Aluno aluno = AlunoRepository.BuscarId(id);
+            AlunoViewModel alunoviewmodel = new AlunoViewModel
+            {
+                AlunoId = aluno.AlunoId,
+                Nome = aluno.Nome,
+                Campus = aluno.Campus,
+                Curso = aluno.Curso,
+                Sexo = aluno.Sexo
+
+            };
+            return View(alunoviewmodel);
+        }
+        #endregion
+
+        #region Methods Buscar
+        public ActionResult Buscar(string pesquisa)
+        {
+            IEnumerable<Aluno> alunos = AlunoRepository.BuscarAluno(pesquisa);
+
+            IEnumerable<AlunoViewModel> alunosviewmodel = alunos.Select(x => new AlunoViewModel
+            {
+                AlunoId = x.AlunoId,
+                Nome = x.Nome,
+                Campus = x.Campus,
+                Curso = x.Curso,
+                Sexo = x.Sexo
+            });
+            return View(alunosviewmodel);
+        }
 
         //[HttpPost]
         //public ActionResult Buscar(string pesquisa)
@@ -149,8 +169,8 @@ namespace DemoGitHub.Controllers
 
         //    }
         //}
-        //#endregion
         #endregion
+
 
     }
 }
